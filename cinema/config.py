ec3c6e517a074eea1743a7f05b58dba85078cb4b
borskaia <borskirina20@gmail.com>
2022-01-20 02:53:53 +0300

    my db

--- cinema/config.py
+++ cinema/config.py
@@ -4,7 +4,7 @@
 # class Config(object):
 #     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-host = "127.0.0.1"
-db_name = "cinema"
-user = "postgres"
-password = "156156"
+host = "ec2-34-255-134-200.eu-west-1.compute.amazonaws.com"
+db_name = "d165dir2ak76p3"
+password = "ab7324e4a10a16997f74f3c2683268daebd690aab2ac4c34d723fc9abf6817c9"
+user = "qwmxwygddrpxxg"
